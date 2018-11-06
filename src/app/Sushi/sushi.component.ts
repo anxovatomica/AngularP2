c99a44d7b896fa235673bae238617740b54b28d4
System Administrator <root@MacBook-Pro-de-Linus.local>
2018-11-06 15:38:46 +0100

    m

--- src/app/Sushi/sushi.component.ts
+++ src/app/Sushi/sushi.component.ts
@@ -1,44 +1,61 @@
 import { Component } from "@angular/core"
 
 @Component(
-    { selector: 'sushi-tag',
-    templateUrl: './sushi.component.html',
-    styleUrls: ['./sushi.component.css'],
-}
+    {
+        selector: 'sushi-tag',
+        templateUrl: './sushi.component.html',
+        styleUrls: ['./sushi.component.css'],
+
+    }
 )
-export class SushiComponent{
-    titulo:string="SOY EL SUSHI COMPONENT";
-    sushi={
-        nombre:"",
-        tipo:"",
-        precio:"",
-        vegano:""
+export class SushiComponent {
+    titulo: string = "SOY EL SUSHI COMPONENT";
+    sushiSelected = 0;
+    nouIngredient="";
+    sushi = {
+        nombre: "",
+        tipo: "",
+        precio: "",
+        vegano: "",
     };
-    sushis=[{
+    sushis = [{
         nombre: "niguiri de salmon",
         tipo: 'nigiri',
         precio: '2€',
-        vegano: 'no'
+        vegano: 'no',
+        ingredients: ["arros, ", "aguacate, ", "salmon, ", "sal, "]
     },
     {
         nombre: "niguiri de atun",
         tipo: 'nigiri',
         precio: '2.5€',
-        vegano: 'no'
+        vegano: 'no',
+        ingredients: ["arros, ", "wasabi, ", "atun, ", "algas, "]
     },
     {
         nombre: "'MAKI' c/o Virgil Abloh 2013",
         tipo: 'Maki',
         precio: '3.5€',
-        vegano: 'yes'
+        vegano: 'yes',
+        ingredients: ["arros, ", "wasabi, ", "aguacate, ", "quinoa, "]
     }
 
     ];
-    borraSushi(index){
+    borraSushi(index) {
         this.sushis.splice(index, 1);
     }
 
-    addSushi(){
+    addSushi() {
         this.sushis.push(JSON.parse(JSON.stringify(this.sushi)));
+    }
+    borraIngredient(index) {
+        console.log("delete");
+        //this.sushis[this.sushiSelected].ingredients.slice(index, 1);
+        let sushiPerBorrar = this.sushis[this.sushiSelected];
+        sushiPerBorrar.ingredients.splice(index,1);
+    }
+    addIngredient(){
+        let sushiAfegir = this.sushis[this.sushiSelected];
+        sushiAfegir.ingredients.push(this.nouIngredient);
     }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SushiComponent } from './Sushi/sushi.component';
 import { PelisComponent } from './Pelis/pelis.component';
 import { Pelis2Component } from './Pelis/pelis2.component';
 import {FormsModule} from '@angular/forms';
@@ -9,6 +10,7 @@ import {FormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
+    SushiComponent,
     PelisComponent,
     Pelis2Component,
   ],
